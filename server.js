eda76cf3b1a645d87a433158126a6f30db8b0e2d
Ibrahim Kolabalogun <32678498+Kolabalogun@users.noreply.github.com>
2023-12-30 08:09:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ const port = 3001;
 app.use(express.json());
 app.use(cors());
 
-app.use("/api", async (req, res) => {
-  //   const url = `https://api.football-data.org/v2${req.url}`; // all matches
-
-  const url = `https://api.football-data.org/v4/competitions/PL/matches?matchday=${req.url} `; // pl matches
+app.use("/api/:matchDay", async (req, res) => {
+  const matchDay = req.params.matchDay; // Access the matchDay parameter from the route
+  console.log(matchDay);
+  const url = `https://api.football-data.org/v4/competitions/PL/matches?matchday=${matchDay}`;
   const response = await fetch(url, {
     headers: {
       "X-Auth-Token": "a621e855bacc4de0b5d86da4ad62a525",
